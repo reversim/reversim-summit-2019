080d4e4ddd31a15f4ab6c43e9d7b0d00ad579049
Nathan Hazout <nathanh@il.ibm.com>
2019-05-16 10:33:19 +0300

    initial calendar sync
    Signed-off-by: Nathan Hazout <nathanh@il.ibm.com>

---
+++ scripts/reversim-calendar/index.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+const calendarId = "78oe494u8ij7qpv4qeko7im03k@group.calendar.google.com";
+const Agenda = require("./agenda");
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
+
+// Load client secrets from a local file.
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Calendar API.
+  authorize(JSON.parse(content), syncData);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("Enter the code from that page here: ", code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error retrieving access token", err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+async function syncData(auth) {
+  await cleanUp(auth);
+  const calendar = google.calendar({ version: "v3", auth });
+  let agenda = new Agenda();
+  await agenda.load();
+
+  let events = agenda.day1.concat(agenda.day2);
+  if (events.length > 0) {
+    for (let event of events) {
+      await calendar.events.insert({
+        calendarId,
+        resource: event
+      });
+    }
+  }
+}
+
+async function cleanUp(auth) {
+  const calendar = google.calendar({ version: "v3", auth });
+  let res = await calendar.events.list({
+    calendarId,
+    showDeleted: false,
+    singleEvents: true
+  });
+  for (let event of res.data.items) {
+    await calendar.events.delete({ calendarId, eventId: event.id });
+  }
+}
